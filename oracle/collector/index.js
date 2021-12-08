b48e51f4e5f4ba7823d02c1ad8dfea322cb6ecc3
Abhishek kochar <43106142+Abhishekkochar@users.noreply.github.com>
2021-12-09 09:30:34 +1300

    Update with Ethereum data

--- oracle/collector/index.js
+++ oracle/collector/index.js
@@ -1,13 +1,16 @@
 const { collect_bsc } = require('./bsc');
 const { collect_solana } = require('./solana');
+const { collect_eth } = require('./ethereum');
 
 async function collect() {
     const bsc = await collect_bsc();
     const solana = await collect_solana();
+    const eth = await collect_eth();
     
     return {
         bsc,
         solana,
+        eth,
     }
 }
 
