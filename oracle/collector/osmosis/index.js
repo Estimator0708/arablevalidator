984928bf93401559c507ef5a2833ac4fb2debfa0
Chris Raastad <craastad@gmail.com>
2021-12-10 16:05:45 -0600

    Updated osmosis collector to use on chain info. Added updated package-lock.json. Added coin ghecko API dependency.

--- oracle/collector/osmosis/index.js
+++ oracle/collector/osmosis/index.js
@@ -1,22 +1,31 @@
 const axios = require('axios')
+const CoinGecko = require('coingecko-api')
+const coinGeckoApi = new CoinGecko();
 
 async function collect_osmosis() {
 
-    const poolData = await axios.get('https://api-osmosis.imperator.co/pools/v1/1').then(response => response.data)
-    console.log('ATOM-OSMO pool data: ' + JSON.stringify(poolData, null, 2))
-    const atom = poolData[0]
-    const osmo = poolData[1]
+    let priceData = (await coinGeckoApi.simple.price({
+        ids: ['cosmos', 'osmosis'],
+        vs_currencies: ['usd'],
+    })).data;
+    console.log('priceData: ' + JSON.stringify(priceData, null, 2))
 
-    if (atom.liquidity != osmo.liquidity) {
-        throw `ATOM ${atom.liquidity} and OSMO ${osmo.liquidity} liquidity do not agree!`
+    let poolData = await axios.get('https://lcd-osmosis.keplr.app/osmosis/gamm/v1beta1/pools/1').then(response => response.data)
+    console.log('pool data: ' + JSON.stringify(poolData, null, 2))
+
+    const atom = poolData.pool.poolAssets[0]
+    const osmo = poolData.pool.poolAssets[1]
+
+    if (atom.weight != osmo.weight) {
+        throw `ATOM ${atom.weight} and OSMO ${osmo.weight} weight do not match!`
     }
-    const totalSupply = atom.liquidity
+    const totalSupply = atom.weight/10.0**6
     
-    const totalTokenOne = atom.amount
-    const atomPrice = atom.price
-    const totalTokenTwo = osmo.amount
-    const osmoPrice = osmo.price
-    const totalLiquidity = totalTokenOne * atomPrice + totalTokenTwo * osmoPrice
+    const totalAtom = atom.token.amount/10.0**6
+    const atomPrice = priceData.cosmos.usd
+    const totalOsmosis = osmo.token.amount/10.0**6
+    const osmoPrice = priceData.osmosis.usd
+    const totalLiquidity = totalAtom * atomPrice + totalOsmosis * osmoPrice
 
     const lpTokenPrice = totalLiquidity/totalSupply
     return {
