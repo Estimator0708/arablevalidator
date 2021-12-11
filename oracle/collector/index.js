f39777fbaafe1dcf940f55133f8ce1824e5fb610
gophereth <gophereth121@gmail.com>
2021-12-11 15:44:13 +1000

    add osmosis collector

--- oracle/collector/index.js
+++ oracle/collector/index.js
@@ -1,13 +1,16 @@
 const { collect_bsc } = require('./bsc');
+const { collect_osmosis } = require('./osmosis');
 const { collect_solana } = require('./solana');
 
 async function collect() {
     const bsc = await collect_bsc();
     const solana = await collect_solana();
+    const osmosis = await collect_osmosis();
     
     return {
         bsc,
         solana,
+        osmosis,
     }
 }
 
