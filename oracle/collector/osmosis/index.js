f366241f7563e068c6fc2a6ec8b87f150e2a3b3d
Chris Raastad <craastad@gmail.com>
2021-12-13 16:24:15 -0600

    Added staking APR calculation

--- oracle/collector/osmosis/index.js
+++ oracle/collector/osmosis/index.js
@@ -7,12 +7,18 @@ const coinGeckoApi = new CoinGecko();
 
 async function collect_osmosis() {
 
+    // TODO: get price from script output https://github.com/ArableProtocol/arablevalidator/issues/4
     let priceData = (await coinGeckoApi.simple.price({
         ids: ['cosmos', 'osmosis'],
         vs_currencies: ['usd'],
-    })).data;
-    //console.log('priceData: ' + JSON.stringify(priceData, null, 2))
+    })).data
 
+    const osmoInfo = await coinGeckoApi.coins.fetch('osmosis',{})
+    // TODO: get total supply from some on chain API call, I can't find it
+    // I'm not sure what the correct supply to use in the staking APR calculation
+    const osmoSupply = osmoInfo.data.market_data.circulating_supply
+    //const osmoSupply = osmoInfo.data.market_data.total_supply
+    
     // compute ATOM-OSMO LP token price
     const poolId = 1 //ATOM-OSMO
     const poolData = await axios.get('https://lcd-osmosis.keplr.app/osmosis/gamm/v1beta1/pools/' + poolId).then(res => res.data.pool)
@@ -25,11 +31,11 @@ async function collect_osmosis() {
     }
     const totalSupply = atom.weight/10.0**6
     
-    const totalAtom = atom.token.amount/10.0**6
+    const totalLpAtom = atom.token.amount/10.0**6
     const atomPrice = priceData.cosmos.usd
-    const totalOsmo = osmo.token.amount/10.0**6
+    const totalLpOsmo = osmo.token.amount/10.0**6
     const osmoPrice = priceData.osmosis.usd
-    const poolTotalValueLocked = totalAtom * atomPrice + totalOsmo * osmoPrice
+    const poolTotalValueLocked = totalLpAtom * atomPrice + totalLpOsmo * osmoPrice
 
     const lpTokenPrice = poolTotalValueLocked/totalSupply
 
@@ -45,18 +51,23 @@ async function collect_osmosis() {
     const epochProvision = await axios.get('https://lcd-osmosis.keplr.app/osmosis/mint/v1beta1/epoch_provisions').then(res => res.data.epoch_provisions)
     
     // lockable durations: https://lcd-osmosis.keplr.app/osmosis/pool-incentives/v1beta1/lockable_durations
-    const rewardApr1day = computeApr('86400s')
-    const rewardApr7days = computeApr('604800s') + rewardApr1day
-    const rewardApr14days = computeApr('1209600s') + rewardApr7days
+    const rewardApr1day = computeLpRewardsApr('86400s')
+    const rewardApr7days = computeLpRewardsApr('604800s') + rewardApr1day
+    const rewardApr14days = computeLpRewardsApr('1209600s') + rewardApr7days
+
+    // compute OSMO staking rewards APR
+    const osmoStakingRewardsApr = computeStakingApr()
 
     return {
+        osmoStakingRewardsApr: osmoStakingRewardsApr,
+        osmoStakingRewardsAprPct: (100.0*osmoStakingRewardsApr).toFixed(2) + '%',
         atomOsmoLpTokenTotalValueLockedUsd: poolTotalValueLocked,
         atomOsmoLpTokenPrice: lpTokenPrice,
         atomOsmoLpTokenReward14DaysBondedApr: rewardApr14days,
         atomOsmoLpTokenReward14DaysBondedAprPct: (100.0*rewardApr14days).toFixed(2) + '%'
     }
 
-    function computeApr(duration) {
+    function computeLpRewardsApr(duration) {
         const gaugeId = incentivizedPools.filter(ip => ip.lockable_duration == duration)[0].gauge_id
         const potWeight = distrInfo.records.filter(r => r.gauge_id == gaugeId)[0].weight
 
@@ -66,15 +77,21 @@ async function collect_osmosis() {
         const epoch = epochs.filter(e => e.identifier == epochIdentifier)[0]
         const epochDuration = dayjs.duration(parseInt(epoch.duration.replace('s', '')) * 1000)
 
-        const numEpochPerYear = dayjs.duration({ years: 1, }).asMilliseconds() / epochDuration.asMilliseconds()
+        const numEpochsPerYear = dayjs.duration({ years: 1, }).asMilliseconds() / epochDuration.asMilliseconds()
 
-        const yearProvision = epochProvision * numEpochPerYear
+        const yearProvision = epochProvision * numEpochsPerYear
         const yearProvisionToPots = yearProvision * mintParams.distribution_proportions.pool_incentives
         const yearProvisionToPot = yearProvisionToPots * (potWeight / totalWeight)
         const yearProvisionToPotPrice = mintPrice * yearProvisionToPot / 10 ** 6
 
-        const rewardApr = yearProvisionToPotPrice / poolTotalValueLocked
-        return rewardApr
+        return yearProvisionToPotPrice / poolTotalValueLocked
+    }
+
+    function computeStakingApr() {
+        const numEpochsPerYear = 365
+        const yearProvision = epochProvision * numEpochsPerYear
+        const yearProvisionToPot = yearProvision * mintParams.distribution_proportions.staking / 10.0 ** 6
+        return yearProvisionToPot / osmoSupply
     }
 }
 
