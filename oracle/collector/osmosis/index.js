053eb6fa06ec00a2a4fec64a4559f2ffe1216f4a
Chris Raastad <craastad@gmail.com>
2021-12-13 12:58:56 -0600

    Fixed calculation and return important info

--- oracle/collector/osmosis/index.js
+++ oracle/collector/osmosis/index.js
@@ -16,7 +16,6 @@ async function collect_osmosis() {
     // compute ATOM-OSMO LP token price
     const poolId = 1 //ATOM-OSMO
     const poolData = await axios.get('https://lcd-osmosis.keplr.app/osmosis/gamm/v1beta1/pools/' + poolId).then(res => res.data.pool)
-    //console.log('pool data: ' + JSON.stringify(poolData, null, 2))
 
     const atom = poolData.poolAssets[0]
     const osmo = poolData.poolAssets[1]
@@ -30,45 +29,45 @@ async function collect_osmosis() {
     const atomPrice = priceData.cosmos.usd
     const totalOsmo = osmo.token.amount/10.0**6
     const osmoPrice = priceData.osmosis.usd
-    const totalLiquidity = totalAtom * atomPrice + totalOsmo * osmoPrice
+    const poolTotalValueLocked = totalAtom * atomPrice + totalOsmo * osmoPrice
 
-    const lpTokenPrice = totalLiquidity/totalSupply
+    const lpTokenPrice = poolTotalValueLocked/totalSupply
 
     // compute LP token reward APY for 14 day bondage
     const distrInfo = await axios.get('https://lcd-osmosis.keplr.app/osmosis/pool-incentives/v1beta1/distr_info').then(res => res.data.distr_info)
-    //console.log('distroInfo: ' + JSON.stringify(distrInfo, null, 2))
     const totalWeight = distrInfo.total_weight
     
     const incentivizedPools = await axios.get('https://lcd-osmosis.keplr.app/osmosis/pool-incentives/v1beta1/incentivized_pools').then(res =>
         res.data.incentivized_pools.filter(ip => ip.pool_id == poolId)
     )
-    //console.log('incentiziedPools: ' + JSON.stringify(incentivizedPools, null, 2))
-    const gaugeId14Days = incentivizedPools.filter(ip => ip.lockable_duration == '1209600s')[0].gauge_id
-    const potWeight = distrInfo.records.filter(r => r.gauge_id == gaugeId14Days)[0].weight
+    
+    const duration1day = '86400s'
+    const duration14days = '1209600s'
+    const gaugeId = incentivizedPools.filter(ip => ip.lockable_duration == duration1day)[0].gauge_id
+    const potWeight = distrInfo.records.filter(r => r.gauge_id == gaugeId)[0].weight
 
     const mintParams = await axios.get('https://lcd-osmosis.keplr.app/osmosis/mint/v1beta1/params').then(res => res.data.params)
-    //console.log('mintParams: ' + JSON.stringify(mintParams, null, 2))
     const mintPrice = osmoPrice
     const epochIdentifier = mintParams.epoch_identifier
 
     const epochs = await axios.get('https://lcd-osmosis.keplr.app/osmosis/epochs/v1beta1/epochs').then(res => res.data.epochs)
-    //console.log('epochs: ' + JSON.stringify(epochs, null, 2))
     const epoch = epochs.filter(e => e.identifier == epochIdentifier)[0]
     const epochDuration = dayjs.duration(parseInt(epoch.duration.replace('s', '')) * 1000);
 
     const epochProvision = await axios.get('https://lcd-osmosis.keplr.app/osmosis/mint/v1beta1/epoch_provisions').then(res => res.data.epoch_provisions)
-    const numEpochPerYear = dayjs.duration({years: 1,}).asMilliseconds() / epochDuration.asMilliseconds();
+    const numEpochPerYear = dayjs.duration({years: 1,}).asMilliseconds() / epochDuration.asMilliseconds()
+
     const yearProvision = epochProvision * numEpochPerYear
     const yearProvisionToPots = yearProvision * mintParams.distribution_proportions.pool_incentives
     const yearProvisionToPot = yearProvisionToPots * (potWeight / totalWeight)
-    const yearProvisionToPotPrice = mintPrice * yearProvisionToPot
+    const yearProvisionToPotPrice = mintPrice * yearProvisionToPot / 10**6
 
-    const poolTotalValueLocked = totalAtom * atomPrice + totalOsmo + osmoPrice
-
-    const rewardApy = yearProvisionToPotPrice / poolTotalValueLocked
+    const rewardApr = yearProvisionToPotPrice / poolTotalValueLocked
     return {
+        atomOsmoLpTokenTotalValueLockedUsd: poolTotalValueLocked,
         atomOsmoLpTokenPrice: lpTokenPrice,
-        atomOsmoLpTokenRewardApy: rewardApy // expect ~APR 117.86%
+        atomOsmoLpTokenRewardApr: rewardApr,
+        atomOsmoLpTokenRewardAprPct: (100.0*rewardApr).toFixed(2) + '%'
     }
 }
 
