df2c4852ce92ca3587be42973859a4d57b995538
Abhishek kochar <abhishekkochar2@Gmail.com>
2021-12-15 02:52:59 +1300

    Updated time in nodecron

--- oracle/index.js
+++ oracle/index.js
@@ -5,11 +5,7 @@ require("dotenv").config();
 
 async function main() {    
     //All scripts will run first second of first minute every hour
-<<<<<<< HEAD
-    await nodeCron.schedule("* * * * *",async function(){
-=======
     await nodeCron.schedule(" 1 * * * *",async function(){
->>>>>>> a5ac0a2370e2e58d8646243f5f4c12b8e4e36d1c
         const data = await collect();
         const onChainPrice = await onChainPriceFeed(data)
         console.log('collection', JSON.stringify(data, null, '\t'));
