b515a0c32d30ad001b952a66b96abe3b2830aa8f
gophereth <gophereth121@gmail.com>
2021-12-30 01:33:10 +1000

    take out admin functionalities

--- tokenvesting/admin.js
+++ tokenvesting/admin.js
@@ -5,9 +5,10 @@ const {
   stakingReleaseFromStakingRoot,
   stakingRootDistributeRewards,
   getValidators,
+  bulkPermitValidatorCreation,
+  setOTCDeal,
 } = require('./utils/index.js');
 
-const nodeCron = require('node-cron');
 const { waitSeconds } = require('../utils/wait');
 require('dotenv').config();
 
@@ -28,25 +29,35 @@ async function runTokenVesting() {
   await waitSeconds(10);
 
   // - DStaking.claimRewardsFromRoot - all the validator - daily - any user (after release)
-  if (process.env.VALIDATOR_ADDRESS) {
-    await dstakingReleaseFromStakingRoot(process.env.VALIDATOR_ADDRESS);
+  const dStakingInfos = await getValidators();
+  for (let i = 0; i < dStakingInfos.length; i++) {
+    await dstakingReleaseFromStakingRoot(dStakingInfos[i].addr);
     await waitSeconds(5);
-  } else {
-    const dStakingInfos = await getValidators();
-    for (let i = 0; i < dStakingInfos.length; i++) {
-      await dstakingReleaseFromStakingRoot(dStakingInfos[i].addr);
-      await waitSeconds(5);
-    }
   }
   console.log('================ finished token vesting flow ================');
 }
 
-async function main() {
-  // this job will only run once a day as the value is hardcode to a day in farming contract.
-  // As of now, this will run at 1st min of 1am everyday
-  await nodeCron.schedule(' 1 1 * * *', async function () {
-    await runTokenVesting();
-  });
+// runTokenVesting();
+
+async function bulkPermitValidators() {
+  bulkPermitValidatorCreation([
+    '0xF834C6Da7AA4aA71d94c2496554e54103492D5ee',
+    '0xB4084F25DfCb2c1bf6636b420b59eda807953769',
+    '0xD734373209BE506275B96Bc7b287CfcC4454550d',
+  ]);
 }
 
-main();
+// bulkPermitValidators();
+
+async function setOtcDeals() {
+  const currentTimestamp = parseInt(Date.now() / 1000);
+  await setOTCDeal(
+    '0x4Ec663E7041C2B0c47c40FC9e1178ad445ab221D',
+    1000,
+    100,
+    currentTimestamp + 864000, // 10 days expiry
+    currentTimestamp + 86400 * 2 // 2 days unlock time
+  );
+}
+
+// setOtcDeals();
