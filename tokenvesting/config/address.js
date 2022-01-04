a8e11f99c8d5d00474c11c5e029679a0371942d7
gophereth <gophereth121@gmail.com>
2022-01-04 13:10:23 +1000

    update otc deal contract

--- tokenvesting/config/address.js
+++ tokenvesting/config/address.js
@@ -4,7 +4,7 @@ const team_distributer = '0x0D5F7057d4b9bc456C6dB764C337b33ab83af72A';
 const staking_root = '0x927633c1ab3F2AeA010EDc16ddF63651588100b5';
 const staking = '0x8512edA1aE41430c97A119EC104F0447c76a748F';
 const farming = '0xa4577fD2fFD4dde52054D284d293732ca9d0c894';
-const otc = '0x6E1b0CBD208dAB41cE498D4840481B09409324D5';
+const otc = '0x77b39A58b77c4CC7991bdec015b3eEEbA349C3fb';
 
 exports.arable_vesting = arable_vesting;
 exports.root_distributer = root_distributer;
