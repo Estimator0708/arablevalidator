abbdad880e3390e92947a3cdd3f1999a4d52bb9a
Altbee <liaojikundever@gmail.com>
2022-01-27 04:16:50 -0600

    update addresses

--- tokenvesting/config/address.js
+++ tokenvesting/config/address.js
@@ -1,10 +1,10 @@
-const arable_vesting = '0xA8667Af4D6657C9042Bf6427FE60510538c5E1E8';
-const root_distributer = '0x0D5F7057d4b9bc456C6dB764C337b33ab83af72A';
-const team_distributer = '0x0D5F7057d4b9bc456C6dB764C337b33ab83af72A';
-const staking_root = '0x927633c1ab3F2AeA010EDc16ddF63651588100b5';
-const staking = '0x8512edA1aE41430c97A119EC104F0447c76a748F';
-const farming = '0xa4577fD2fFD4dde52054D284d293732ca9d0c894';
-const otc = '0x77b39A58b77c4CC7991bdec015b3eEEbA349C3fb';
+const arable_vesting = '0x567b32C78D6275e3575Daa676BbDC3720123B0c8';
+const root_distributer = '0xD0a96D7E8765EB9fb3702246AbA7F5e5e42B4a0B';
+const team_distributer = '0x6b16Ea70e82DefDe1bE123224C124cdBC9A065Cf';
+const staking_root = '0x78f1442f97dbacC79F1d4C74d3097Dc460d93672';
+const staking = '0x96C726cC2Fe6121f8f69ea31594B33f14Ab00d11';
+const farming = '0x82bd5ba2ad6aDF7aa1bCb78527d2ab00F6956094';
+const otc = '0x363f67e27224779c77633C5296ca22dA2b614693';
 
 exports.arable_vesting = arable_vesting;
 exports.root_distributer = root_distributer;
